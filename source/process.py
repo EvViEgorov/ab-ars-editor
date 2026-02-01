55ec8e9a3f0746c3198cb3add73adb83d0e54a21
EvViEgorov <egorovevgeniy7a@gmail.com>
2026-02-01 20:13:27 +0300

    Improved sigfile parsing function, added data-to-sigfile writing function

--- source/process.py
+++ source/process.py
@@ -1,8 +1,17 @@
 import re
+import json
 import demjson3
+import jstyleson
 
 
-def parse_sig_file(text: str, text_only=False):
+class SigData:
+    def __init__(self, data, text_head, text_data):
+        self.data = data
+        self.text_head = text_head
+        self.text_data = text_data
+
+
+def parse_sig_file(text: str) -> dict:
     # счет x-ов
     def eval_str(match):
         expr = match.group()
@@ -15,13 +24,27 @@ def parse_sig_file(text: str, text_only=False):
     sig_head = re.match(pattern, text, re.DOTALL | re.VERBOSE)
     if sig_head:
         sig_head = sig_head.group()
-
-    sig_data = re.sub(pattern, '', text, flags=re.DOTALL | re.VERBOSE)
-    # считаем иксы для нормального парсинга
-    sig_data = re.sub(r"(?<=x: ).+?(?=,)", eval_str, sig_data)
-
-    parsed = demjson3.decode(sig_data)
-    if text_only:
-        return sig_head, sig_data
+        sig_data = re.sub(pattern, '', text, flags=re.DOTALL | re.VERBOSE)
+        # считаем иксы для нормального парсинга
+        sig_data = re.sub(r"(?<=x: ).+?(?=,)", eval_str, sig_data)
     else:
-        return parsed
+        sig_data = None
+    try:
+        parsed = demjson3.decode(sig_data)
+    except demjson3.JSONDecodeError:
+        parsed = None
+
+    sig_data = {
+        'data': parsed,
+        'text': {
+            'head': sig_head,
+            'rawdata': sig_data,
+        },
+    }
+    sig_data = SigData(parsed, sig_head, sig_data)
+    return sig_data
+
+
+def data_to_file(sig_data: SigData) -> str:
+    text = sig_data.text_head + json.dumps(sig_data.data, indent=4, ensure_ascii=False)
+    return text
